aa157f3045d0249e1c6dbbc5c9fca2df9425ce04
Iran Anzola <iranjo23@gmail.com>
2021-04-21 18:14:36 -0400

    Primer versionamiento del proyecto cb-systemtest

---
+++ app/Http/Livewire/Pages.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Models\Page;
+use Livewire\Component;
+
+class Pages extends Component
+{
+    public $modalFormVisible = false;
+    public $slug;
+    public $title;
+    public $content;
+    
+    /**
+     * Show de form modal of the create function.
+     *
+     * @return void
+     */
+    public function createShowModal()
+    {
+        $this->modalFormVisible = true;
+    }
+    
+    /**
+     * The livewire render function.
+     *
+     * @return void
+     */
+    public function render()
+    {
+        return view('livewire.pages');
+    }
+}
